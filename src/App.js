fff56338c01ead1866128c6383e350f869408e1c
unknown <nitinsivam.a2021@vitstudent.ac.in>
2023-11-01 01:12:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Calculator from './Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Calculator} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
 
 
